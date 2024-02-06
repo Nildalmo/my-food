8db2bf26a8b6ddb734a10f518af95cb8b5b7a1a2
nildalmo <2nildalmo@gmail.com>
2024-02-05 22:03:44 -0300

    feat:commit adicionar

---
+++ src/components/Input.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { TextInputProps, View } from "react-native";
+import { forwardRef } from "react";
+import { AuthTextInputLabel, AuthTextInput } from "../app/styles/auth.style";
+
+export interface inputProps extends TextInputProps {
+  label: string;
+}
+
+const BaseStyle = ({ label, ...rest }: inputProps, ref: any) => {
+  return (
+    <View>
+      <AuthTextInputLabel>{label}</AuthTextInputLabel>
+      <AuthTextInput ref={ref} {...rest} />
+          </View>
+  );
+};
+
+export const Input = forwardRef(BaseStyle);
